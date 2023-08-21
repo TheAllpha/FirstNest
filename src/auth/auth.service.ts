f2192c73d64eb5c2615bb999e26b864453d7cdec
kerem <kerem_bolt@hotmail.com>
2023-08-21 10:51:19 +0300

    docker

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from "@nestjs/common";
 
 @Injectable({})
-export class AuthService{}
+export class AuthService{
+    signup() {
+        return { msg: 'You have signed up' }
+    }
+
+    signin() { 
+    return { msg :'You have signed in' }
+    }
+}
